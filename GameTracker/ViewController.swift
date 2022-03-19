938724d592ec15a9f1a7436bbd54cf11e4aeb205
VladGolosoff <v.golosoff@gmail.com>
2022-03-19 21:12:08 +0300

    Initial Commit

---
+++ GameTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GameTracker
+//
+//  Created by Владислав Голосов on 19.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
